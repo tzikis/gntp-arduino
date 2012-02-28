6a9a3910f70955793a7a938f1f5d60e006fde886
Vasileios Georgitzikis <georgitzik@ceid.upatras.gr>
2012-02-29 01:55:03 +0200

    notification sending added

--- GNTP.cpp
+++ GNTP.cpp
@@ -1,9 +1,21 @@
 #include "GNTP.h"
 
-GNTP::GNTP(EthernetClient client, IPAddress server)
+
+GNTPNotification::GNTPNotification(char *name, char *title, char *description)
+{
+	this->title = title;
+	this->description = description;
+	this->name = name;
+}
+
+GNTP::GNTP(char *appName, EthernetClient client, IPAddress server)
 {
 	this->client=client;
 	this->server=server;
+	this->appName = appName;
+	this->myNotifications = (notificationsList*) malloc(sizeof(notificationsList));
+	this->myNotifications->nextNotification = NULL;
+	this->port = DEFAULT_PORT;
 	// init("unknown");
 }
 
@@ -26,15 +38,33 @@ void GNTP::checkConnection()
   }
 }
 
-void GNTP::sendNotification()
+void GNTP::sendNotification(char* name)
 {
-  if(client.connect(server, 23053))
+	notificationsList* lastItem = myNotifications;
+	while(lastItem->nextNotification != NULL)
+	{
+		lastItem = lastItem->nextNotification;
+		if(!strcmp(lastItem->notification.name, name))
+		{
+			sendNotification(lastItem->notification);
+			break;
+		}
+	}	
+}
+
+void GNTP::sendNotification(GNTPNotification notification)
+{
+  if(client.connect(server, this->port))
   {
     client.println("GNTP/1.0 NOTIFY NONE MD5 7E99EC79CEF859867F40FE88E22075F3.E9B99BC5E617AD81");
-    client.println("Application-Name: SurfWriter ");
-    client.println("Notification-Name: Download Complete ");
-    client.println("Notification-Text: Hello!");
-    client.println("Notification-Display-Name: Download completed ");
+    client.print("Application-Name: ");
+	client.println(appName);
+    client.print("Notification-Name: ");
+	client.println(notification.name);
+    client.print("Notification-Text: ");
+	client.println(notification.description);
+    client.print("Notification-Title: ");
+	client.println(notification.title);
     client.println();
     client.println();
     delay(50);
@@ -42,17 +72,62 @@ void GNTP::sendNotification()
   }
 }
 
-void GNTP::registerNotification()
+void GNTP::registerNotifications()
 {
+	Serial.println("connecting...");
 	// Make a HTTP request:
-	client.println("GNTP/1.0 REGISTER NONE MD5 7E99EC79CEF859867F40FE88E22075F3.E9B99BC5E617AD81");
-	client.println("Application-Name: SurfWriter"); 
-	client.println("Notifications-Count: 1");
-	client.println();
-	client.println("Notification-Name: Download Complete ");
-	client.println("Notification-Enabled: True");
-	client.println();
-	client.println();
-	delay(50);
-	checkConnection();
+	if(client.connect(server, this->port))
+	{
+		Serial.println("connected");
+		client.println("GNTP/1.0 REGISTER NONE MD5 7E99EC79CEF859867F40FE88E22075F3.E9B99BC5E617AD81");
+		client.print("Application-Name: "); 
+		client.println(appName);
+		int bla = count();
+		client.print("Notifications-Count: ");
+		client.println(bla);
+		
+		notificationsList* lastItem = myNotifications;
+		while(lastItem->nextNotification != NULL)
+		{
+			lastItem = lastItem->nextNotification;
+			client.println();
+			client.print("Notification-Name: ");
+			client.println(lastItem->notification.name);
+			client.println("Notification-Enabled: True");
+		}
+		client.println();
+		client.println();		
+		delay(50);
+		checkConnection();
+	}
+	else
+	{
+		// kf you didn't get a connection to the server:
+		Serial.println("connection failed");
+	}
+}
+
+void GNTP::add(GNTPNotification notification)
+{
+	notificationsList* lastItem = myNotifications;
+	while(lastItem->nextNotification != NULL)
+	{
+		lastItem = lastItem->nextNotification;
+	}
+	notificationsList* newNotification = (notificationsList*) malloc(sizeof(notificationsList));
+	newNotification->nextNotification = NULL;
+	newNotification->notification = notification;
+	lastItem->nextNotification = newNotification;
+}
+
+int GNTP::count(void)
+{
+	notificationsList* lastItem = myNotifications;
+	int count = 0;
+	while(lastItem->nextNotification != NULL)
+	{
+		count++;
+		lastItem = lastItem->nextNotification;
+	}
+	return count;
 }
--- GNTP.h
+++ GNTP.h
@@ -6,15 +6,41 @@
 #include "Ethernet.h"
 #define do{ NEWLINE Serial.println("");}while(false)
 
+
+class GNTPNotification
+{
+public:
+	char* title;
+	char* description;
+	char* name;
+	GNTPNotification(char *name, char *title, char *description);
+};
+
+struct notificationsList
+{
+	GNTPNotification notification;
+	struct notificationsList* nextNotification;
+};
+
+typedef struct notificationsList notificationsList;
+
 class GNTP
 {
 public:
-	GNTP(EthernetClient client, IPAddress server);
-	void registerNotification(void);
+	const static int DEFAULT_PORT = 23053;
+	GNTP(char *appName, EthernetClient client, IPAddress server);
+	void add(GNTPNotification notification);
+	void registerNotifications(void);
 	void checkConnection(void);
-	void sendNotification(void);
+	void sendNotification(char* name);
+	void sendNotification(GNTPNotification notification);
+	int count(void);
 	EthernetClient client;
 	IPAddress server;
+	char* appName;
+	notificationsList* myNotifications;
+private:
+	int port;
 };
 
 #endif
